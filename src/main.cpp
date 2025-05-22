6003de60ff749bce2e62b375b85cad7335e69519
izasoerya <ihza.surya@gmail.com>
2025-05-22 23:17:15 +0700

    feat: add mqtt dep

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 #include <TaskScheduler.h>
 #include <time.h>
 
 #include "wifi_service.h"
 #include "flowmeter.h"
 #include "models.h"
+#include "kalman.h"
 
 void taskReading();
 void taskSending();
@@ -15,16 +17,57 @@ void taskSending();
 Task reading(1000, TASK_FOREVER, &taskReading);
 Task sending(10000, TASK_FOREVER, &taskSending);
 
+KalmanFilter kalman; // Create instance
+PayloadDeviceName payloadDeviceName("Flowmeter-1");
 WiFiClientSecure wClient;
 WiFiService wifiService;
 FlowMeter flowMeter;
 Scheduler scheduler;
 PayloadData payloadData;
-PayloadDeviceName payloadDeviceName("Flowmeter-1");
 
-const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 7;
-const int daylightOffset_sec = 3600;
+// MQTT setup
+WiFiClient mqttNetClient;
+PubSubClient mqttClient(mqttNetClient);
+const char *mqtt_broker = "103.150.117.46";
+const uint16_t mqtt_port = 1883;
+const char *mqtt_user = "arr1";
+const char *mqtt_pass = "arr1";
+const char *mqtt_client_id = "Flowmeter-Sani";
+String lastPayloadData = "";
+bool useKalmanFilter = true; // Toggle filtering
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+	String data;
+	for (unsigned int i = 0; i < length; i++)
+	{
+		data += (char)payload[i];
+	}
+	if (data == "START")
+		lastPayloadData = "START";
+	else if (data == "STOP")
+		lastPayloadData = "STOP";
+}
+
+void mqttReconnect()
+{
+	while (!mqttClient.connected())
+	{
+		Serial.print("Attempting MQTT connection...");
+		if (mqttClient.connect(mqtt_client_id, mqtt_user, mqtt_pass))
+		{
+			Serial.println("connected");
+			mqttClient.subscribe("your/topic");
+		}
+		else
+		{
+			Serial.print("failed, rc=");
+			Serial.print(mqttClient.state());
+			Serial.println(" try again in 5 seconds");
+			delay(5000);
+		}
+	}
+}
 
 void IRAM_ATTR ISR_function()
 {
@@ -33,42 +76,59 @@ void IRAM_ATTR ISR_function()
 
 void setup()
 {
+	pinMode(32, INPUT_PULLUP);
 	Serial.begin(115200);
 	wClient.setInsecure();
-	wifiService.connect(wClient);
+	wifiService.connect();
+
+	mqttClient.setServer(mqtt_broker, mqtt_port);
+	mqttClient.setCallback(mqttCallback);
+	mqttReconnect();
 
 	scheduler.init();
 	scheduler.addTask(reading);
-	scheduler.addTask(sending);
 	reading.enable();
-	sending.enable();
 
-	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-	attachInterrupt(digitalPinToInterrupt(32), ISR_function, RISING); // Attach interrupt to pin 2
+	attachInterrupt(digitalPinToInterrupt(32), ISR_function, RISING);
 }
 
 void loop()
 {
 	wifiService.reconnect();
 	scheduler.execute();
+	if (!mqttClient.connected())
+	{
+		mqttReconnect();
+	}
+	mqttClient.loop();
 }
 
 void taskReading()
 {
-	time_t now = time(nullptr);
-	struct tm *timeinfo = localtime(&now);
-	char timestampStr[32];
-	strftime(timestampStr, sizeof(timestampStr), "%Y-%m-%dT%H:%M:%SZ", gmtime(&now));
+	if (lastPayloadData == "START")
+	{
+		flowMeter.resetPulseCount();
+		lastPayloadData = "";
+	}
 
-	if (timeinfo->tm_year + 1900 >= 2024)
+	if (lastPayloadData == "STOP")
 	{
 		HTTPClient http;
+
+		// Get volume in mL
+		float volume = flowMeter.getVolumeMilliLiters();
+		float filteredVolume = useKalmanFilter ? kalman.filter(volume) : volume;
+
+		// Get doc ID and set payload
 		uint32_t currentId = wifiService.getDocument(http, wClient);
 		payloadData.setLogId(currentId + 1);
-		payloadData.setValue(flowMeter.getPulseCount());
-		payloadData.setTimeStamp(String(timestampStr));
+		payloadData.setValue(volume); // Use mL value, cast to integer
+		payloadData.setValueKalman(filteredVolume);
+		flowMeter.resetPulseCount();
+
+		taskSending();
+		lastPayloadData = "";
 	}
-	flowMeter.resetPulseCount();
 }
 
 void taskSending()
