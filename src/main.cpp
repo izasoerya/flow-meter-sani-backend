592ed6a7cd4577b0f7455abe6ad09d013124f56f
izasoerya <ihza.surya@gmail.com>
2025-06-21 14:35:58 +0700

    feat: change weighting

--- src/main.cpp
+++ src/main.cpp
@@ -131,8 +131,8 @@ void taskReading()
 		uint32_t currentId = wifiService.getDocument(http, httpSecureClient); // gunakan klien terpisah
 
 		payloadData.setLogId(currentId + 1);
-		payloadData.setValue(flowRateLPM * 3 * 0.784);
-		payloadData.setValueKalman(filteredFlow * 3 * 0.784);
+		payloadData.setValue(flowRateLPM * 3 * 0.782);
+		payloadData.setValueKalman(filteredFlow * 3 * 0.782);
 
 		JsonDocument docData = payloadData.toJson();
 		JsonDocument docName = payloadDeviceName.toJson();
